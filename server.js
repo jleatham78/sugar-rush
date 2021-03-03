2712cbcbb183fe979bafd79250f0aa3090f9c0ba
Robert Clawson <bobbycharizard@gmail.com>
2021-03-02 22:07:14 -0700

    Heroku issue

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: process.env.SECRET,
+  secret: 'mycatisoverweight',
   cookie: {},
   resave: false,
   saveUninitialized: true,
