c8c503d7056bb2183f82ecfd18abba323004eca1
Staci Genesi <sgenesi@hotmail.com>
2021-03-04 12:45:00 -0700

    playing with drink cards

---
+++ public/javascript/drinks.js
@@ -0,0 +1,12 @@
+// function displayDrinks(baseInput) {
+//     const menuData = "../../seed";
+//     console.log('Yo');
+//     menuData.filter(function (baseInput) {
+//         return Base.base == baseInput;
+//     });
+// }
+
+function displayCoke() {
+    var C = document.getElementById("cokes");
+    C.style.display = "block";
+}
